53baa8360ce7be177247ffb9edc85c910a1e1ccd
Sara Edrees <122927746+Saraedrees@users.noreply.github.com>
2023-02-17 04:21:26 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@
 /* This program prints exactly "Programming is like building a multilingual puzzle */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
