525d830e5a5586d9493d3aa85b5dcbaaef09edbc
Sara Edrees <122927746+Saraedrees@users.noreply.github.com>
2023-02-17 23:09:19 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,19 +6,19 @@
  */
 int main(void)
 {
-	int n;
+	double n;
 
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%lf is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%lf is zero\n", n);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%lf is negative\n", n);
 	}
 	return (0);
 }
