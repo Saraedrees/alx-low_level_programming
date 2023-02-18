e394b69ab8a6671caa6c60a902bec7e19456ed4e
saraedrees <sara.mostafa_m@yahoo.com>
2023-02-17 18:07:48 -0800

    base16 lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print the base16 numbers in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+	int num;
+
+	for (num = 0; num < 10 ; num++)
+	{
+		putchar(num);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
