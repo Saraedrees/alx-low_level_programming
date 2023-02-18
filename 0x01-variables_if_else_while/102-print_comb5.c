9b362bbfae3c7e5d6f348d5ee781c8e82ca64621
Sara Edrees <122927746+Saraedrees@users.noreply.github.com>
2023-02-18 04:45:48 +0200

    Create 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print all combinations of single-digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+        int num;
+
+        for (num = 0; num < 10; num++)
+        {
+                putchar((num % 10) + '0');
+                if ((num % 10) < 9)
+                {
+                putchar(',');
+                putchar(' ');
+                }
+        }
+        putchar('\n');
+        return (0);
+}
