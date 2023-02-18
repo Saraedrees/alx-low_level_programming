abd4148efb89e72809110067611b8bfde018b722
saraedrees <sara.mostafa_m@yahoo.com>
2023-02-18 11:49:54 -0800

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Print exact sentence, followed by a new line,
+ * to the standard error
+ *
+ * Return: 1 (Success)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	return (1);
+}
