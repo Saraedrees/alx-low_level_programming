2e9ebb62b342501441ea274cd8f3716775c46c60
Sara Edrees <122927746+Saraedrees@users.noreply.github.com>
2023-02-18 03:53:35 +0200

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - print all single digits from 0 to 10
+ * main - print all single digits from 0 to 9
  *
  * Return: Always 0 (success)
  */
@@ -11,8 +11,6 @@
 int main(void)
 {
 	int num;
-
-	num = getchar();
 
 	for (num = 0; num < 10; num++)
 	{
