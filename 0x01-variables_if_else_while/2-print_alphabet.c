edcc7598e2d3e098afaa55f282020f96d7a55c97
saraedrees <sara.mostafa_m@yahoo.com>
2023-02-17 16:03:09 -0800

    alphapet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+
+int main (void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+}
