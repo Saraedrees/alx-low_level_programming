34841c539ceea80a2ed11c1e655eb44d2e0e3319
saraedrees <sara.mostafa_m@yahoo.com>
2023-02-17 17:20:13 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print all single digits from 0 to 10
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; c++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
