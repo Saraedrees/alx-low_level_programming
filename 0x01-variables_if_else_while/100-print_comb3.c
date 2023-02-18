2d8a683cc077cafc530de9bb1af88b19b9aff290
saraedrees <sara.mostafa_m@yahoo.com>
2023-02-17 23:45:14 -0800

    combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,17 +15,17 @@ int main(void)
 
 	for (a = 0; a < 9; a++)
 	{
-		for (b = a + 1; b < 10; b++)
-		{
-			putchar((a % 10) + '0');
-			putchar((b % 10) + '0');
-			if (a == 8 && b == 9)
-				continue;
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
+	for (b = a + 1; b < 10; b++)
+	{
+	putchar((a % 10) + '0');
+	putchar((b % 10) + '0');
+	if (a == 8 && b == 9)
+	continue;
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
 	}
 	putchar('\n');
 	return (0);
