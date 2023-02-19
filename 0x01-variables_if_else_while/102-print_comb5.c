df9ac1b7642700996df6e5e9786a02200a6bb992
saraedrees <sara.mostafa_m@yahoo.com>
2023-02-18 16:25:15 -0800

    two 2-digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,17 +3,31 @@
 #include <stdio.h>
 
 /**
- * main - print all combinations of 
+ * main - print all combinations of two two-digit numbers
+ * ranging from 00 to 99, with space between 2 numbers,
+ * and with comma and space after them
  *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-        int num;
+	int a, b;
 
+	for (int a = 0; a < 99; a++)
+	{
+		for (b = a + 1; b < 100; b++)
+		{
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b % 10) + '0');
 
-        }
-        putchar('\n');
-        return (0);
+			if (a == 98 && b == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	return (0);
+	}
 }
