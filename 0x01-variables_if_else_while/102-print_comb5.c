9d1e95af0f5cfe480b2265f5401e24b768baa613
saraedrees <sara.mostafa_m@yahoo.com>
2023-02-20 14:27:38 -0800

    two two-digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,8 +18,10 @@ int main(void)
 	{
 		for (b = a + 1; b < 100; b++)
 		{
+			putchar((a / 10) + '0');
 			putchar((a % 10) + '0');
 			putchar(' ');
+			putchar((b / 10) + '0');
 			putchar((b % 10) + '0');
 
 			if (a == 98 && b == 99)
