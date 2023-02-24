3d6a03d3c58eae6dbc9dbbf5b4360c5c878072f0
saraedrees <sara.mostafa_m@yahoo.com>
2023-02-23 16:08:48 -0800

    24 hr

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -20,10 +20,6 @@ void jack_bauer(void)
 			_putchar(':');
 			_putchar((b / 10) + '0');
 			_putchar((b % 10) + '0');
-
-			if (a == 23 && b == 59)
-				continue;
-			_putchar('\n');
 			_putchar('\n');
 		}
 	}
