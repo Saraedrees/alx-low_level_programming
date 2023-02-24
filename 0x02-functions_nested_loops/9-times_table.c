07fcf8a6332132d8d05fbde49b9f7284311314bb
saraedrees <sara.mostafa_m@yahoo.com>
2023-02-23 16:48:52 -0800

    times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,13 +12,21 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		_putchar(0);
+
+		for (b = 1; b <= 9; b++)
 		{
-			r = a * b;
-			_putchar((r / 10) + '0');
-			_putchar((r % 10) + '0');
 			_putchar(',');
 			_putchar(' ');
+
+			r = a * b;
+
+			if (r <= 9)
+				_putchar(' ');
+			else
+				_putchar((r / 10) + '0');
+
+			_putchar((r % 10) + '0');
 		}
 		_putchar('\n');
 	}
