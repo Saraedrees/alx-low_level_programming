90e31ea5f5cfb9f5aeb0d111205b0c774b7e17f3
saraedrees <sara.mostafa_m@yahoo.com>
2023-02-24 11:43:43 -0800

    print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,18 +4,21 @@
  * print_most_numbers - print numbers from 0 to 9,
  * followed by a new line,
  * except 2 and 4.
+ * @a: number to be checked.
  *
  * Return: no return.
  */
 
 void print_most_numbers(void)
 {
+	int a;
+
 	for (a = 0; a <= 9; a++)
 	{
 		if (a != 2 && a != 4)
 		{
 			_putchar((a % 10) + '0');
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
