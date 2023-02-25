102d3091959b89a1f02555dfbcf05b0631a3644a
saraedrees <sara.mostafa_m@yahoo.com>
2023-02-25 11:16:20 -0800

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,16 +2,15 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0.
- * @n: number of times table
+ * @n: number of times table (0 < n <= 15)
  *
  */
 
 void print_times_table(int n);
 {
+	int i, j, r;
 
-	int i, j;
-
-	if (n > 15 && n < 0)
+	if (n <= 0 && n >= 15)
 	{
 		break;
 	}
@@ -20,14 +19,34 @@ void print_times_table(int n);
 		for (i = 0; i <= n; i++)
 		{
 			_putchar('0');
-			for (j = 0; j <= n; j++)
+
+			for (j = 1; j <= n; j++)
 			{
-				_putchar(i * j);
+				r = i * j;
 				_putchar(',');
 				_putchar(' ');
+
+				if (r <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(r + '0');
+				}
+				else if (r <= 99)
+				{
+					_putchar(' ');
+					_putchar((r / 10) + '0');
+					_putchar((r % 10) + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(((r / 100) % 10) + '0');
+					_putchar(((r % 100) % 10) + '0');
+					_putchar((r % 10) + '0');
+				}
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
-
