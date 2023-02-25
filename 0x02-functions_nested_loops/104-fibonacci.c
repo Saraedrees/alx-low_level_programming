57ac7eb505682f88396ec8c568c998917cc57e3a
saraedrees <sara.mostafa_m@yahoo.com>
2023-02-25 14:20:00 -0800

    long fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ *
+ * Return: 0 (Success).
+ */
+
+int main(void)
+{
+	long unsigned int n1, n2, sum;
+	int i;
+
+	n1 = 0;
+	n2 = 1;
+	for (i = 1; i <= 98; i++)
+	{
+		sum = n1 + n2;
+		n1 = n2;
+		n2 = sum;
+
+		if (i == 98)
+		{
+			printf("%lu", sum);
+		}
+		else
+		{
+			printf("%lu, ", sum);
+		}
+	}
+	printf("\n");
+	return (0);
+}
