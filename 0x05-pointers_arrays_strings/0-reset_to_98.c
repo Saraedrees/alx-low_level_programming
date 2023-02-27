ec7b4c2ecf30d67d28269a62c331edfab6887705
saraedrees <sara.mostafa_m@yahoo.com>
2023-02-27 15:26:38 -0800

    reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -12,11 +12,8 @@ void reset_to_98(int *n)
 
 	i = 402;
 	n = &i;
-	_putchar((i / 100) + '0');
-	_putchar((i / 10) + '0');
-	_putchar((i % 10) + '0');
-	_putchar('\n');
 	*n = 98;
+	_putchar((i / 100) + '0');
 	_putchar((i / 10) + '0');
 	_putchar((i % 10) + '0');
 	_putchar('\n');
