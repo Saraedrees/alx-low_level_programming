d06c29b48f3754a1d4a31421190f4d90d8fad88d
saraedrees <sara.mostafa_m@yahoo.com>
2023-02-28 08:45:00 -0800

    print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,13 +11,11 @@ void print_rev(char *s)
 	int len = 0, index = 0;
 	char tmp;
 
-	while (s[index++])
-		len++;
-
-	for (index = len - 1; index >= len / 2; index--)
-	{
-		tmp = s[index];
-		s[index] = s[len - index - 1];
-		s[len - index - 1] = tmp;
-	}
+		for (index = len - 1; index >= len / 2; index--)
+		{
+			tmp = s[index];
+			s[index] = s[len - index - 1];
+			s[len - index - 1] = tmp;
+			_putchar(s[index]);
+		}
 }
