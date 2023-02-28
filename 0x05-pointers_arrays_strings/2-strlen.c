34d21f53c706417dd7e5c8160c33066de498581b
saraedrees <sara.mostafa_m@yahoo.com>
2023-02-27 17:01:58 -0800

    string_length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,15 +2,11 @@
 
 /**
  * strlen - function that returns the length of a string.
- * @s:
+ * @s: string to be checked.
  *
  * Return: length value of a string.
  */
 int _strlen(char *s)
 {
-	char l;
-
-	l = *s;
-	return (l);
+	return (s);
 }
-	
