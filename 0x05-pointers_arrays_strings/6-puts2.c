bbf62f1e4cc656351614745baabdba92a1f1a28c
Sara Edrees <122927746+Saraedrees@users.noreply.github.com>
2023-03-01 02:07:50 +0200

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,14 +12,17 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	if (i == '\0')
+	while (i >= 0)
 	{
-		_putchar('\n');
-		break;
-	}
-	if (i % 2 == 0)
-	{
-		_putchar(str[i]);
-		i++;
+		if (str[i] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+			i++;
+		}
 	}
 }
