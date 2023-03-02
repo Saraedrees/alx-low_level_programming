9d945f3cef73f2bbb2af9742fb234a68ec0b71e0
saraedrees <sara.mostafa_m@yahoo.com>
2023-03-02 07:25:33 -0800

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,12 +12,7 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (i >= 0)
-	{
+	for (i = 0; src[i] != '\0'; i++)
 		dest[i] = src[i];
-		if (src[i] == '\0')
-			break;
-		i++;
-	}
 	return (dest);
 }
