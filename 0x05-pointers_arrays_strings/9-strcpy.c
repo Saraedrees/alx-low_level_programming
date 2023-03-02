818f87cf5d375738a2fcd98deef91c95dbfda4b6
Sara Edrees <122927746+Saraedrees@users.noreply.github.com>
2023-03-02 17:27:08 +0200

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,7 +12,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	for (i = 0; src[i] != '\0'; i++)
+	while (i >= 0)
+	{
 		dest[i] = src[i];
+		if (src[i] == '\0')
+			break;
+		i++;
+	}
 	return (dest);
 }
