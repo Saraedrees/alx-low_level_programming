bb1d24f8275b2b8a96dbb6187423f7ad1c757b4d
saraedrees <sara.mostafa_m@yahoo.com>
2023-03-01 22:32:12 -0800

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,16 +9,16 @@
 
 void print_triangle(int size)
 {
-        int tria, i;
+	int tria, i;
 
-        for (tria = 0; tria < size; tria++)
-        {
-                for (i = 1; i < (size - tria); i++)
-                        _putchar(' ');
-                for (i--; i < size; i++)
-                        _putchar('#');
-                if (tria < (size - 1))
-                        _putchar('\n');
-        }
-        _putchar('\n');
+	for (tria = 0; tria < size; tria++)
+	{
+		for (i = 1; i < (size - tria); i++)
+			_putchar(' ');
+		for (i--; i < size; i++)
+			_putchar('#');
+		if (tria < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
 }
