e0b5009c9da92724add698769e7015224f99c5ed
saraedrees <sara.mostafa_m@yahoo.com>
2023-03-05 11:11:32 -0800

    update

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,36 +8,19 @@
  */
 void print_number(int n)
 {
-	int i;
+	unsigned int i = n;
 
 	if (n < 0)
-		_putchar(-n);
+	{
+		_putchar('-');
+		i = i * -1;
+	}
 	else
 	{
-		for (i = 0; i >= 0; i++)
-		{
-			if (n >= 0 && n <= 9)
-				_putchar((n % 10) + '0');
-			else if (n >= 10 && n <= 99)
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-			}
-			else if (n >= 100 && n <= 999)
-			{
-				_putchar((n / 100) + '0');
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-			}
-			else if (n >= 1000 && n <= 9999)
-			{
-				_putchar((n / 1000) + '0');
-				_putchar((n / 100) + '0');
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-			}
-			else
-				continue;
-		}
+		n = n;
 	}
+	if ((i / 10) > 0)
+		_putchar(i / 10);
+
+	_putchar((i % 10) + '0');
 }
