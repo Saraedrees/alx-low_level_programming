78da0f714ef886d61b388c4c24297040b5e8245f
Sara Edrees <122927746+Saraedrees@users.noreply.github.com>
2023-03-08 02:29:47 +0200

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -17,7 +17,7 @@ char *_strstr(char *haystack, char *needle)
 		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (haystack[i] == needle[j])
-				return (needle);
+				return (needle + j);
 		}
 	}
 	return ('\0');
