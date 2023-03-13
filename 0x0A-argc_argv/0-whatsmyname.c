30df2acdee1c3e57e66d1180e7962e6dcd721131
saraedrees <sara.mostafa_m@yahoo.com>
2023-03-13 16:00:08 -0700

    argc,argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * main - prints name of the program followed by new line
+ * @argc: argument count
+ * @argv: arguments vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H__
+#define _MAIN_H__
+
+int _putchar(char c);
+
+#endif
