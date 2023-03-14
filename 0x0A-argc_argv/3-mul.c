7d18b4b367e0f98756c529a6111cb5d70cd8c53c
saraedrees <sara.mostafa_m@yahoo.com>
2023-03-13 17:52:04 -0700

    argc,argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int n1, n2, mul;
+	int n1, n2;
 
 	if (argc != 3)
 	{
@@ -19,7 +19,6 @@ int main(int argc, char *argv[])
 	}
 	n1 = atoi(argv[1]);
 	n2 = atoi(argv[2]);
-	mul = n1 * n2;
-	printf("%d\n", mul);
+	printf("%d\n", n1 * n2);
 	return (0);
 }
