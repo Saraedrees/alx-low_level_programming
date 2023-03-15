edfa126b841b7423414298087bbac023cb96ef85
Sara Edrees <122927746+Saraedrees@users.noreply.github.com>
2023-03-15 04:30:21 +0200

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 
 	if (w <= 0 || h <= 0)
 		return (NULL);
-	ptr = malloc(sizeof(int) * width);
+	ptr = malloc(sizeof(int) * width * height);
 	for (w = 0; w < width; w++)
 	{
 		for (h = 0; h < height; h++)
