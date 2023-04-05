d99b4d7ae997b4726ef251a5a00cb2bd44900e88
Sara Edrees <122927746+Saraedrees@users.noreply.github.com>
2023-04-05 06:37:44 +0200

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,7 +5,7 @@
  * free_listint2 - function that frees a listint_t list.
  * @head: pointer to pointer to head of the listint_t list.
  * The function sets the head to NULL.
- * 
+ *
  * Return: void.
  */
 void free_listint2(listint_t **head)
