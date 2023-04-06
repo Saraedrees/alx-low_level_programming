60ddf277bbdbaa772ba6179fa28e15680ddadd65
Sara Edrees <122927746+Saraedrees@users.noreply.github.com>
2023-04-06 04:29:15 +0200

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,56 +2,56 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-void print_char(va_list args);
-void print_integer(va_list args);
-void print_float(va_list args);
-void print_string(va_list args);
+void print_char(va_list arg);
+void print_integer(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
 
 /**
  * print_char - Prints a char.
- * @args: A list of arguments pointing to the character to be printed.
+ * @arg: A list of arguments pointing to the character to be printed.
  */
-void print_char(va_list args)
+void print_char(va_list arg)
 {
 	char letter;
 
-	letter = va_arg(args, int);
+	letter = va_arg(arg, int);
 	printf("%c", letter);
 }
 
 /**
  * print_int - Prints an int.
- * @args: A list of arguments pointing to the integer to be printed.
+ * @arg: A list of arguments pointing to the integer to be printed.
  */
-void print_int(va_list args)
+void print_int(va_list arg)
 {
 	int num;
 
-	num = va_arg(args, int);
+	num = va_arg(arg, int);
 	printf("%d", num);
 }
 
 /**
  * print_float - Prints a float.
- * @args: A list of arguments pointing to the float to be printed.
+ * @arg: A list of arguments pointing to the float to be printed.
  */
-void print_float(va_list args)
+void print_float(va_list arg)
 {
 	float num;
 
-	num = va_arg(args, double);
+	num = va_arg(arg, double);
 	printf("%f", num);
 }
 
 /**
  * print_string - Prints a string.
- * @args: A list of arguments pointing to the string to be printed.
+ * @arg: A list of arguments pointing to the string to be printed.
  */
-void print_string(va_list args)
+void print_string(va_list arg)
 {
 	char *str;
 
-	str = va_arg(args, char *);
+	str = va_arg(arg, char *);
 
 	if (str == NULL)
 	{
@@ -94,7 +94,7 @@ void print_all(const char * const format, ...)
 		{
 			printf("%s", separator);
 			funcs[j].print(args);
-			separator = ",";
+			separator = ", ";
 		}
 		i++;
 	}
