d8ba1316328e8fe5f12381b85f5bd6d6d4fedcb2
Sara Edrees <122927746+Saraedrees@users.noreply.github.com>
2023-04-06 04:37:22 +0200

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include "calc.h"
+#include "3-calc.h"
 
 /**
  * get_op_func - a function that selects the correct function to
@@ -11,3 +11,18 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
+op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
+
+while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+
+	return (ops[i].f);
+}
