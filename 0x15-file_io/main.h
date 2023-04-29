f7b0118443be5a17903868c96b2004f7612818da
Sara Edrees <122927746+Saraedrees@users.noreply.github.com>
2023-04-29 03:59:10 +0200

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,10 +6,24 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <elf.h>
+#include <stdio.h>
+
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
+void chk_elf(unsigned char *e_id);
+void prnt_magic(unsigned char *e_id);
+void prnt_class(unsigned char *e_id);
+void prnt_data(unsigned char *e_id);
+void prnt_version(unsigned char *e_id);
+void prnt_abi(unsigned char *e_id);
+void prnt_osabi(unsigned char *e_id);
+void prnt_type(unsigned int e_typ, unsigned char *e_id);
+void prnt_entry(unsigned long int e_enty, unsigned char *e_id);
+void close_elf(int elf);
 
 #endif
