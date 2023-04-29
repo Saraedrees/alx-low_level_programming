b651e0711292e61f2bd6142b54e1643744c972e1
Sara Edrees <122927746+Saraedrees@users.noreply.github.com>
2023-04-29 04:30:24 +0200

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,7 +9,6 @@
 #include <elf.h>
 #include <stdio.h>
 
-
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
@@ -25,5 +24,7 @@ void prnt_osabi(unsigned char *e_id);
 void prnt_type(unsigned int e_typ, unsigned char *e_id);
 void prnt_entry(unsigned long int e_enty, unsigned char *e_id);
 void close_elf(int elf);
+
+int main(int __attribute__((__unused__)) ac, char *av[]);
 
 #endif
